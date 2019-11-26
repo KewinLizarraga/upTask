57076ae75e0bf4d96def66621f26cb9895d0b654
KewinLizarraga <kewin392@gmail.com>
2019-11-26 18:37:55 -0500

    [DELETE] email

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: '07822ff8e87524',
-    pass: '3e3933647b3d3a',
-    host: 'smtp.mailtrap.io',
-    port: '2525'
+    user: '<user>',
+    pass: '<password>',
+    host: '<host>',
+    port: '<port>' 
 }
